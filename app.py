cb4167ffe7c01280a40fdd12a350644c92d3768e
Nishit Mohanan <nishitmohanan@gmail.com>
2019-02-17 01:07:38 +0530

    Added test route in flask

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/test")
+def test():
+    return "Hey This works!!!"
+
+if __name__ == "__main__":
+    app.run()
