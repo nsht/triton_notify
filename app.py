899e9f6d8daa975cadda4472507152495eca76af
Nishit Mohanan <nishitmohanan@gmail.com>
2019-02-18 00:25:31 +0530

    Added send route

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask,make_response
 
 app = Flask(__name__)
+MESSAGE_TYPES = ["telegram", "email", "sms", "log", "twitter"]
+
+
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/test")
-def test():
-    return "Hey This works!!!"
+
+@app.route("/test/<name>")
+def test(name):
+    return f"Hey {name}"
+
+
+@app.route("/send/<message_type>", methods=["GET", "POST"])
+def send(message_type):
+    if message_type not in MESSAGE_TYPES:
+        return make_response("Type not found", 404)
+    return f"{message_type} sent successfully"
+
 
 if __name__ == "__main__":
     app.run()
