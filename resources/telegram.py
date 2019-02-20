e405e99422790fe13536723cd2bcb0fdc296d7ee
Nishit Mohanan <nishitmohanan@gmail.com>
2019-02-21 00:01:08 +0530

    Modified error response

--- resources/telegram.py
+++ resources/telegram.py
@@ -8,14 +8,14 @@ class Telegram:
         self.message = message_data.get("message")
         self.token = message_data.get("token")
         self.chat_id = message_data.get("chat_id")
-        if not all([self.message,self.token,self.chat_id]):
-            return {"message_status":False,"status_message":"Invalid Request","status_code":400}
+
 
     def send_message(self):
-        print("message sent desu")
+        if not all([self.message,self.token,self.chat_id]):
+            return {"message_status":"Not Sent","status_message":"Invalid Request","status_code":400}
         telegram = get_notifier('telegram')
         message_status = telegram.notify(message=self.message,token=self.token,chat_id=self.chat_id)
-        print(message_status)
+        import pdb;pdb.set_trace()
         if message_status.errors:
-            return {"message_status":False,"status_message":"Unable to send message","status_code":500}
-        return {"message_status":True,"status_message":"Message Send Successfully","status_code":200}
+            return {"message_status":"Not Sent","status_message":"Unable to send message reason:{}".format(message_status.errors[0]),"status_code":message_status.response.status_code}
+        return {"message_status":"Sent","status_message":"Message Sent Successfully","status_code":message_status.response.status_code}
