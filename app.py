cc3e95c7cfa3d11a48bd288953446f5ee9ab3ee8
Nishit Mohanan <nishitmohanan@gmail.com>
2019-03-01 01:02:27 +0530

    Added user and userpermissions model

--- app.py
+++ app.py
@@ -23,12 +23,39 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     user_type = db.Column(db.String(20), nullable=False)
     email_id = db.Column(db.String(120), unique=True, nullable=False)
-    date_of_creation = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
+    date_created = db.Column(
+        db.TIMESTAMP(timezone=True), nullable=False, default=datetime.datetime.utcnow
+    )
     last_login = db.Column(db.TIMESTAMP(timezone=True))
     status = db.Column(db.Integer, nullable=False)
+    user_permissions = db.relationship("UserPermissions ", backref="user", lazy=True)
 
     def __repr__(self):
         return f"Username: {self.username}"
+
+class Permissions(db.Model):
+    perm_id = db.Column(db.Integer, primary_key=True)
+    perm_name = db.Column(db.String(50), unique=True, nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+    date_created = db.Column(
+        db.TIMESTAMP(timezone=True), nullable=False, default=datetime.datetime.utcnow
+    )
+
+    def __repr__(self):
+        return f"Permission: {perm_name}"
+
+
+class UserPermissions(db.Model):
+    uperm_id = db.Column(db.Integer, primary_key=True)
+    perm_id = db.Column(db.Integer, db.ForeignKey("permissions.perm_id"), nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.uid"), nullable=False)
+    access_granted_on = db.Column(
+        db.TIMESTAMP(timezone=True), nullable=False, default=datetime.datetime.utcnow
+    )
+
+    def __repr__(self):
+        return f"Permission{perm_id}, User={user_id}"
 
 
 # Adds RotatingFileHandler if app is not running on aws lambda
