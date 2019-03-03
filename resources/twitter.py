d1837532d000d3a6009b031949185a285e2e91d4
Nishit Mohanan <nishitmohanan@gmail.com>
2019-03-04 01:05:26 +0530

    Added repr and len functions

--- resources/twitter.py
+++ resources/twitter.py
@@ -10,5 +10,10 @@ class Twitter:
         self.account_handle = message_data.get("account_handle")
         self.consumer_key = message_data.get("consumer_key")
         self.consumer_secret= message_data.get("consumer_secret")
-        
+
+    def __len__(self):
+        return len(self.message)
+
+    def __repr__(self):
+        return "{!r}({!r})".format(self.__class__.__name__, self.message_data)
 
