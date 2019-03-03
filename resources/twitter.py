a15cc4dbcdc1d83819d6b345f45f9f5fa78c1d24
Nishit Mohanan <nishitmohanan@gmail.com>
2019-03-04 01:05:04 +0530

    Added twitter init variables

--- resources/twitter.py
+++ resources/twitter.py
@@ -2,6 +2,13 @@ import tweepy
 
 
 class Twitter:
-    def __init__():
-        print("init")
+    def __init__(self, message_data, request_obj):
+        print(message_data)
+        self.message_data = message_data
+        self.request_obj = request_obj
+        self.message = message_data.get("message")
+        self.account_handle = message_data.get("account_handle")
+        self.consumer_key = message_data.get("consumer_key")
+        self.consumer_secret= message_data.get("consumer_secret")
+        
 
