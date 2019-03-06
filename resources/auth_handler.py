c0160b3a3a470e1864acebce55b5f629044fd8ef
Nishit Mohanan <nishitmohanan@gmail.com>
2019-03-07 01:52:36 +0530

    Add function to create jwt tokens

--- resources/auth_handler.py
+++ resources/auth_handler.py
@@ -5,6 +5,7 @@ from flask import current_app as app
 from flask import request
 from resources.constants import *
 from flask_restful import abort
+import jwt
 
 
 def check_message_type(func):
@@ -19,3 +20,19 @@ def check_message_type(func):
             abort(404, message=f"Invalid message type {message_type}")
 
     return inner
+
+
+def create_auth_token(user_id):
+    payload = {
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+        "iat": datetime.datetime.utcnow(),
+        "user_id": user_id,
+    }
+    try:
+        token = jwt.encode(payload, "secret", algorithm="HS256").decode()
+        return token
+    except Exception as e:
+        app.logger.error(
+            f"{datetime.datetime.utcnow().isoformat()} |Error generating token:{e} | {request.remote_addr}"
+        )
+        return
