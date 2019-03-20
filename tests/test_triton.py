126ae210f99fd56f3685fc568195a59a7b02c431
Nishit Mohanan <nishitmohanan@gmail.com>
2019-03-21 01:14:14 +0530

    Added login tests

--- tests/test_triton.py
+++ tests/test_triton.py
@@ -1,25 +1,39 @@
 import os
 import tempfile
+import json
 
 import pytest
 
-import triton_notify
+import triton_notify, pdb
+from triton_notify.models import models
+import bcrypt
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def client():
-    db_fd, triton_notify.app.config["DATABASE"] = tempfile.mkstemp()
+    # Create in memory db
+    triton_notify.app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///:memory:"
+    models.db.create_all()
     triton_notify.app.config["TESTING"] = True
     client = triton_notify.app.test_client()
 
+    # create test password hash
+    test_pasword = "correct horse battery staple".encode()
+    test_password_hash = bcrypt.hashpw(test_pasword, bcrypt.gensalt())
+    # Create User object
+    test_user = models.User(
+        username="test",
+        password=test_password_hash,
+        user_type="test_user",
+        email_id="test@example.com",
+        status=1,
+    )
+    models.db.session.add(test_user)
+    models.db.session.commit()
     yield client
 
-    os.close(db_fd)
-    os.unlink(triton_notify.app.config["DATABASE"])
 
-
-def test_empty_db(client):
-    """Start with a blank database."""
+def test_index(client):
     response = client.get("/")
     assert b"ok" in response.data
 
@@ -35,3 +49,24 @@ def test_unauthorized_message_send(client):
     response = client.post("/message/telegram")
     assert response.status_code == 401
 
+
+def test_successfull_login(client):
+    """Test login """
+    test_password = "correct horse battery staple"
+    response = client.post(
+        "/login",
+        data=json.dumps({"username": "test", "password": test_password}),
+        content_type="application/json",
+    )
+    assert response.status_code == 200
+
+
+def test_unsuccessfull_login(client):
+    """Test Failed login conditions"""
+    test_password = "incorrect horse battery staple"
+    response = client.post(
+        "/login",
+        data=json.dumps({"username": "test", "password": test_password}),
+        content_type="application/json",
+    )
+    assert response.status_code == 401
