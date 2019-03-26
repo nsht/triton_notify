8e84324fdbb20b109565cc509877b0e5ddd52870
nishit <nishitmohanan@gmail.com>
2019-03-27 00:58:35 +0530

    Moved ses token check to send email function

--- triton_notify/resources/email.py
+++ triton_notify/resources/email.py
@@ -19,6 +19,7 @@ class Email:
         self.subject = message_data.get("subject", "New Notification Triton")
         self.message = message_data.get("message")
         self.message_html = message_data.get("message_html")
+        # TODO move these and send_email_ses functions to a seperate class
         self.region_name = os.environ.get("AWS_REGION_NAME", "us-east-1")
         self.aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
         self.aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
@@ -31,16 +32,6 @@ class Email:
         return "{!r}({!r})".format(self.__class__.__name__, self.message_data)
 
     def send_message(self):
-        if not all([self.aws_access_key_id, self.aws_secret_access_key]):
-            app.logger.error(
-                f"{datetime.datetime.utcnow().isoformat()} | AWS keys not set; message_data:{self.message_data} | {self.request_obj.remote_addr}"
-            )
-            return {
-                "message_status": "Not Sent",
-                "status_message": "Server Error",
-                "status_code": 500,
-            }
-
         if not all([self.message, self.sender, self.recipient]):
             app.logger.error(
                 f"{datetime.datetime.utcnow().isoformat()} | Invalid Request:{self.message_data} | {self.request_obj.remote_addr}"
@@ -63,6 +54,15 @@ class Email:
         return template
 
     def send_email_ses(self, recipient, sender, subject, message, message_html):
+        if not all([self.aws_access_key_id, self.aws_secret_access_key]):
+            app.logger.error(
+                f"{datetime.datetime.utcnow().isoformat()} | AWS keys not set; message_data:{self.message_data} | {self.request_obj.remote_addr}"
+            )
+            return {
+                "message_status": "Not Sent",
+                "status_message": "Server Error",
+                "status_code": 500,
+            }
         client = boto3.client("ses", region_name=self.region_name)
         try:
             response = client.send_email(
